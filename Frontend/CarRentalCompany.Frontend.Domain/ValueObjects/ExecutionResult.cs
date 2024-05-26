7e8ce4268adc6434a7ebe925fcec822e9fe99427
Marlena Piotrowska <marlena.daria.piotrowska1996@gmail.com>
2024-05-26 09:20:04 +0200

    Add new projects to solution

---
+++ Frontend/CarRentalCompany.Frontend.Domain/ValueObjects/ExecutionResult.cs
@@ -0,0 +1,26 @@
+﻿namespace CarRentalCompany.Frontend.Domain.ValueObjects
+{
+    public class ExecutionResult
+    {
+        public bool IsSuccess { get; }
+        public string Message { get; }
+
+        protected ExecutionResult(
+            bool isSuccess,
+            string message)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+
+        public static ExecutionResult CreateSuccessful()
+        {
+            return new ExecutionResult(true, null);
+        }
+
+        public static ExecutionResult CreateFailed(string message)
+        {
+            return new ExecutionResult(false, message);
+        }
+    }
+}
---
+++ Frontend/CarRentalCompany.Frontend.Domain/ValueObjects/ExecutionResultGeneric.cs
@@ -0,0 +1,25 @@
+﻿namespace CarRentalCompany.Frontend.Domain.ValueObjects
+{
+    public class ExecutionResultGeneric<TResult> : ExecutionResult
+    {
+        public TResult Payload { get; }
+
+        private ExecutionResultGeneric(
+            bool isSuccess,
+            string message,
+            TResult payload) : base(isSuccess, message)
+        {
+            Payload = payload;
+        }
+
+        public static ExecutionResultGeneric<TResult> CreateSuccessful(TResult payload)
+        {
+            return new ExecutionResultGeneric<TResult>(true, null, payload);
+        }
+
+        public static ExecutionResultGeneric<TResult> CreateFailed(string message)
+        {
+            return new ExecutionResultGeneric<TResult>(false, message, default);
+        }
+    }
+}
